8e6ce0fda9672c254073427e0d1a4327987040f5
AthulDilip <athul@fantacode.com>
2022-02-25 23:08:31 +0530

    Adding controller input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float rotationSpeed = 500f;
 
+    [Header("Ground Check Settings")]
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] Vector3 groundCheckOffset;
+    [SerializeField] LayerMask groundLayer;
+
+    bool isGrounded;
+
+    float ySpeed;
     Quaternion targetRotation;
 
     CameraController cameraController;
     Animator animator;
+    CharacterController characterController;
     private void Awake()
     {
         cameraController =  Camera.main.GetComponent<CameraController>();
         animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -28,9 +38,23 @@ public class PlayerController : MonoBehaviour
 
         var moveDir = cameraController.PlanarRotation * moveInput;
 
+        GroundCheck();
+        if (isGrounded)
+        {
+            ySpeed = -0.5f;
+        }
+        else
+        {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * moveSpeed;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
         if (moveAmount > 0)
         {
-            transform.position += moveDir * moveSpeed * Time.deltaTime;
             targetRotation = Quaternion.LookRotation(moveDir);
         }
 
@@ -38,5 +62,16 @@ public class PlayerController : MonoBehaviour
             rotationSpeed * Time.deltaTime);
 
         animator.SetFloat("moveAmount", moveAmount, 0.2f, Time.deltaTime);
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius, groundLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.5f);
+        Gizmos.DrawSphere(transform.TransformPoint(groundCheckOffset), groundCheckRadius);
     }
 }
